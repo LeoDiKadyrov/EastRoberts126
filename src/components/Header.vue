742c85cb8da96fda3d5c4485a1ff893c00f3f482
Arystan <din02winchester25@gmail.com>
2021-09-02 12:30:12 -0400

    Добавление стилей

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex justify-around items-center">
-        <h1 class="text-justify m-5 italic font-serif text-5xl font-bold">Danny's Housing</h1>
+        <h1 class=" text-justify m-5 italic font-serif text-5xl font-bold">Danny's Housing</h1>
         <div class="font-semibold">
             <a href="#" class="ml-7 hover:text-green-500">West</a>
             <a href="#" class="ml-7 hover:text-green-500">East</a>
@@ -14,3 +14,5 @@ export default {
     name: "Header"
 }
 </script>
+<style scoped>
+</style>
