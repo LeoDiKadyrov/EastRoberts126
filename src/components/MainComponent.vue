88d0656cd365919104dd2e0201c207c7d11ce730
Arystan <din02winchester25@gmail.com>
2021-09-02 12:30:12 -0400

    Добавление роутинга и редактирования
    Теперь у каждой сущности, есть кнопка редактировать, которая перенаправляет на другую страницу и позволяет редактировать человека

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,34 +1,46 @@
 <template>
     <div class="main-div h-screen text-center flex justify-around items-center">
         <div class="w-max">
-            <p class="text-white font-medium font-sans w-5/12 mb-2 text-2xl">
+            <p class="main__subheader text-white font-medium font-sans w-5/12 mb-2 text-2xl">
             Welcome to Danny's house!
             I am landlord from Wildwood. I have been doing this for more than 10 years.
             </p>
-            <p class="text-white font-medium font-sans w-5/12 text-xs mb-2">Rent is $100 a week for each bed.
-            There is a $200 deposit to be given at time of rental.</p>
-            <p class="text-white font-medium font-sans w-5/12 text-xs mb-2">Comforts: Wi-Fi, comfortable common living area, dining room, complete kitchen and bathroom</p>
-            <p class="text-white font-medium font-sans w-5/12 text-xs">East Roberts Ave 126. - Wildwood, New Jersey, 08260</p>
+            <div class="main__text">
+                <p class="text-white font-medium w-5/12 text-xs mb-2">Rent is $100 a week for each bed.
+                There is a $200 deposit to be given at time of rental.</p>
+                <p class="text-white font-medium w-5/12 text-xs mb-2">Comforts: Wi-Fi, comfortable common living area, dining room, complete kitchen and bathroom</p>
+                <p class="text-white font-medium w-5/12 text-xs">East Roberts Ave 126. - Wildwood, New Jersey, 08260</p>
+            </div>
         </div>
-        <div class="bg-black text-white flex w-1/5 justify-center rounded">
+        <div class="main__form text-white flex w-1/5 justify-center rounded">
             <form action="https://getform.io/f/99a5dd2b-8888-4232-a0f7-bf52a2b95931" method="POST">
-                <div>
+                <div class="form__contacts">
                     <h3>Contact me</h3>
-                    <p>Mobile: +1 609 425 7758</p>
+                    <p>Mobile: +1 609 425 7758 (Whatsapp)</p>
                     <p>Email: jppolini101@gmail.com</p>
                 </div>
-                <div class="text-black flex flex-col m-2">
+                <div class="text-black flex flex-col m-2 form__text">
                     <input type="text" placeholder="Your Name" class="mb-2" name="name">
                     <input type="email" placeholder="Your E-mail" class="mb-2" name="email">
                     <input type="text" placeholder="Phone Number" class="mb-2" name="phone">
-                    <textarea name="info" id="1" cols="30" rows="10" class="mb-2">
-When contacting me please include information such as:
-- Date and length of your stay
-- Are male or female
+                    <div class="form__genders">
+                        <input type="radio" name="gender" id="gender-male" value="male"> 
+                        <label for="gender-male" class="mx-2">Male</label>
+                        <input type="radio" name="gender" id="gender-female" value="female"> 
+                        <label for="gender-female" class="mx-2">Female</label>
+                    </div>
+                    <div class="form__dates my-2 flex flex-col">
+                        <label for="date-arrival">Arrival date: </label>
+                        <input type="date" id="date-arrival" name="arrival date">
+                        <label for="date-departure">Departure date: </label>
+                        <input type="date" id="date-departure" name="departure date">
+                    </div>
+                    <input type="text" name="number of people" id="num-people" placeholder="How many people are coming?">
+                    <textarea name="info" id="1" cols="30" rows="10" class="my-2" placeholder="When contacting me please include information such as:
 - Single or couple
 - If you are a group, How many in your group and gender of each
-- How did you find us</textarea>
-                    <button class="bg-white">Send Message</button>
+- How did you find us"></textarea>
+                    <button class="bg-white form__submit">Send Message</button>
                 </div>
             </form>
         </div>
@@ -43,8 +55,26 @@ export default {
 
 <style scoped>
 .main-div {
-    background: url("../assets/street_view.jpg") center no-repeat;
+    background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("../assets/street_view.jpg") center no-repeat;
     background-size: cover;
+}
+.main__subheader {
+    font-family: "Source Sans Pro";
+}
+.main__text {
+    font-family: "Open Sans";
+}
+.main__form {
+    background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ); 
+}
+.form__contacts {
+    color: #987654;
+}
+.form__text {
+    color: #afaeae;
+}
+.form__submit {
+    font-family: "Roboto";
 }
 </style>
 
